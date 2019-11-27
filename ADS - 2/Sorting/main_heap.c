1a9d2827d1529251a8fbee26f5a13a864462a55b
Chandanakoppal <39043121+Chandanakoppal@users.noreply.github.com>
2019-11-27 22:09:40 +0530

    Update main_heap.c

--- ADS - 2/Sorting/main_heap.c
+++ ADS - 2/Sorting/main_heap.c
@@ -1,17 +1,39 @@
-//heapmain
+#include <stdint.h>
+#include <assert.h>
+#include "heapsort.c"
 
-#include<stdio.h>
-#include<stdint.h>
-#include "heapsort.h"
+void test_sort( int32_t data[], uint32_t len) 
+{
+	uint32_t idx;
+	for (idx = 1; idx <= len; ++idx) {
+		assert (data[idx] <= data[idx+1]);		
+	}
+}
+
+void test_heap()
+{
+	int32_t data[] = { 0, 23, 54, 7, 19, 87, 56, 93, 61, 17, 73};
+	Heap heap = heap_new(data, 10);	
+	heap_insert(&heap, 47);
+	heap_test(&heap);
+	assert(heap_get_max(&heap) == 93);
+	assert(heap_extract_max(&heap) == 93);
+	heap_test(&heap);
+	assert(heap_size(&heap) == 10);
+
+	heap_sort(&heap);
+	test_sort(heap.data, 10);
+	
+	int i;
+	for(i = 0; i<11; i++)
+{
+	printf("%d\t", heap.data[i]);
+}
+
+}
+
 int main()
 {
-	int32_t a[10]={30,10,20,90,15,40};
-	Heap h = heap_new(a,6);
-	Heap *heap = &h;
-	Heap *hp = heap_sort(heap);
-	for(int i=0;i<6;i++)
-	{
-		printf("%d\t",heap->data[i]);
-	}
+	test_heap();
 	return 0;
 }
